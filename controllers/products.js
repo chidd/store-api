2233cc0c9abd134196b36aeef51338fa6808ae17
chinedu <chi.adams54@gmail.com>
2021-11-19 21:33:57 +0100

    added numeric Filters for filter by price or rating options

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) =>{
-    const {featured,company,name,sort, fields} = req.query
+    const {featured,company,name,sort, fields, numericFilters} = req.query
     const queryObject = {}
 
     if(featured){
@@ -15,9 +15,32 @@ const getAllProducts = async (req, res) =>{
     if(name){
         queryObject.name = {$regex: name, $options: 'i'}
     }
+
+    // for numeric filtering
+    if(numericFilters){
+        const operatorMap = {
+            '>' : '$gt',
+            '>=' : '$gte',
+            '=' : '$eq',
+            '<' : '$lt',
+            '<=' : '$lte',
+        }
+        const regEx = /\b(<|>|<=|=|>=)\b/g
+        // converting filter operators eg > <= to the ones understood by mongoose
+        let filters  = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
+        
+        const options  = ['price', 'rating']
+        filters = filters.split(',').forEach(item => {
+            const [field,operator,value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator]:Number(value)}
+            }            
+        });
+        
+    }
     
 
-    //console.log(queryObject)
+    
     // const products = await Product.find(queryObject)//this will return a list of products because of the await hence the line below
     let result = Product.find(queryObject)
 
@@ -35,6 +58,12 @@ const getAllProducts = async (req, res) =>{
         const fieldList = fields.split(',').join(' ')
         result = result.select(fieldList)
     } 
+
+    const page = Number(req.query.page) || 1// req.query always returns strings hence the cast using Number()
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
 
     const products = await result
     res.status(200).json({products, nbHits:products.length})
