d74181916f174a490caabd1d9739974a6d021fc8
chinedu <chi.adams54@gmail.com>
2021-11-19 12:15:58 +0100

    added select conditions, sorting and select fields option

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,48 @@
+const Product = require('../models/product')
+
 const getAllProducts = async (req, res) =>{
-    res.status(200).json({msg:"Product route"})
+    const {featured,company,name,sort, fields} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true'?true:false
+    }
+    
+    if(company){
+        queryObject.company = company
+    }
+
+    if(name){
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+    
+
+    //console.log(queryObject)
+    // const products = await Product.find(queryObject)//this will return a list of products because of the await hence the line below
+    let result = Product.find(queryObject)
+
+    // sorting query results
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createdAt')//default sort
+    }
+
+    //selecting specific fields
+    if(fields){
+        const fieldList = fields.split(',').join(' ')
+        result = result.select(fieldList)
+    } 
+
+    const products = await result
+    res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProductsStatic = async (req, res) =>{
-    res.status(200).json({msg:"Product testing route"})
+    const products = await Product.find({})
+    res.status(200).json({products, nbHits:products.length})
 }
 
 module.exports = {getAllProducts, getAllProductsStatic}
